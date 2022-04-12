7d5d28457dffe2471fb031e1607d0a84451c4852
Shaw <eastwoodshaw@gmail.com>
2022-04-12 01:20:34 +0100

    ux: fix typo

--- src/App.svelte
+++ src/App.svelte
@@ -104,7 +104,7 @@
 <main>
 	<h1>
     Word Finder
-    <span  title="sort {alphabetic?'alphabeticically':'ranked'}" on:click="{() => alphabetic = !alphabetic}">
+    <span  title="sort {alphabetic?'alphabetically':'ranked'}" on:click="{() => alphabetic = !alphabetic}">
     <svg xmlns="http://www.w3.org/2000/svg" class="sort-button {alphabetic ? 'ranked' :''}" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12" />
     </svg>
