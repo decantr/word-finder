9af5e7808bce88ba79a60adb008419a8960847cd
Shaw <eastwoodshaw@gmail.com>
2022-04-12 22:31:17 +0100

    ux: improve list view and cleanup code

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,28 @@
 <script lang="ts">
-  import {WordList} from './words';
+	import {WordList} from './words';
 
-  const words: string[] = WordList.words;
+	const words: string[] = WordList.words;
+	const currentGameNo: number = Math.floor((new Date().getTime() - new Date('2021-06-19').getTime()) / (1000 * 3600 * 24));
 
-  const currentGameNo: number = Math.floor((new Date().getTime() - new Date('2021-06-19').getTime()) / (1000 * 3600 * 24));
-
-  let confirmedClicks: number = 0;
-
+	let confirmedClicks: number = 0;
 	let absent: string = '';
-
 	let is_0: string = '';
 	let is_1: string = '';
 	let is_2: string = '';
 	let is_3: string = '';
 	let is_4: string = '';
-
 	let not_0: string = '';
 	let not_1: string = '';
 	let not_2: string = '';
 	let not_3: string = '';
 	let not_4: string = '';
-
-	let nots: string[] = [];
-
+	let nots: string = '';
 	let letterMap: number[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	words.forEach(w => w.split('').forEach(l=>letterMap[l.charCodeAt(0) - 97]++));
 
 	export let couldBe: string[] = [];
 	export let wordScores: {word: string, score: number}[] = [];
 	export let hasSearched: boolean = false;
-	export let alphabetic: boolean = false;
-	export const bestWord: string = findBestWord(words)[0].word;
+	export let showAll: boolean = false;
 
 	function lookFor(){
 		hasSearched = true;
@@ -39,144 +31,141 @@
 		nots = (not_0 + not_1 + not_2 + not_3 + not_4);
 
 		couldBe = words
-			.filter(word => {
-				if ( c.every(l => word.includes(l)) && !ic.some(l => word.includes(l)) ) {
-					if (is_0 !== '' && word[0] !== is_0) return false;
-					if (is_1 !== '' && word[1] !== is_1) return false;
-					if (is_2 !== '' && word[2] !== is_2) return false;
-					if (is_3 !== '' && word[3] !== is_3) return false;
-					if (is_4 !== '' && word[4] !== is_4) return false;
+				.filter(word => {
+					if ( c.every(l => word.includes(l)) && !ic.some(l => word.includes(l)) ) {
+						if (is_0 !== '' && word[0] !== is_0) return false;
+						if (is_1 !== '' && word[1] !== is_1) return false;
+						if (is_2 !== '' && word[2] !== is_2) return false;
+						if (is_3 !== '' && word[3] !== is_3) return false;
+						if (is_4 !== '' && word[4] !== is_4) return false;
 
-					if (not_0 !== '' && not_0.includes(word[0])) return false;
-					if (not_1 !== '' && not_1.includes(word[1])) return false;
-					if (not_2 !== '' && not_2.includes(word[2])) return false;
-					if (not_3 !== '' && not_3.includes(word[3])) return false;
-					if (not_4 !== '' && not_4.includes(word[4])) return false;
-					return true;
-				}
-				return false;
-		});
+						if (not_0 !== '' && not_0.includes(word[0])) return false;
+						if (not_1 !== '' && not_1.includes(word[1])) return false;
+						if (not_2 !== '' && not_2.includes(word[2])) return false;
+						if (not_3 !== '' && not_3.includes(word[3])) return false;
+						if (not_4 !== '' && not_4.includes(word[4])) return false;
+						return true;
+					}
+					return false;
+				});
 
 		if (couldBe.length === words.length) {
 			couldBe = [];
 			hasSearched = false;
 		}
 
-		if (!alphabetic) {
-			letterMap = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-			couldBe.forEach(w => w.split('').forEach(l=>letterMap[l.charCodeAt(0) - 97]++));
-			wordScores = findBestWord(couldBe);
-		}
+		letterMap = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+		couldBe.forEach(w => w.split('').forEach(l=>letterMap[l.charCodeAt(0) - 97]++));
+		wordScores = findBestWord(couldBe);
 	}
 
 	function findBestWord(wordlist: string[]){
-			return wordlist.map(w => {
-				let score = 0;
-				for (const l of w) {
-					if (w.match(new RegExp(l, 'g')).length === 1)
-						score += letterMap[l.charCodeAt(0) - 97];
-					else
-						score -= letterMap[l.charCodeAt(0) - 97];
+		return wordlist.map(w => {
+			let score = 0;
+			for (const l of w) {
+				if (w.match(new RegExp(l, 'g')).length === 1)
+					score += letterMap[l.charCodeAt(0) - 97];
+				else
+					score -= letterMap[l.charCodeAt(0) - 97];
 
-					if (['a','e','i','o','u'].includes(l))
-						score += wordlist.length;
-				}
-				return {word: w, score};
-			}).sort((a,b) => a.score < b.score ? 1 : -1);
+				if (['a','e','i','o','u'].includes(l))
+					score += wordlist.length;
+			}
+			return {word: w, score};
+		}).sort((a,b) => a.score < b.score ? 1 : -1);
 
 	}
 
-  function lowercase(node: HTMLInputElement): void {
-    node.addEventListener('input', () => node.value = node.value.toLowerCase(), { capture: true })
-  }
+	function lowercase(node: HTMLInputElement): void {
+		node.addEventListener('input', () => node.value = node.value.toLowerCase(), { capture: true })
+	}
 
-  function isWordUsed(word: string): boolean {
-    return words.findIndex(w => w === word) < currentGameNo;
-  }
+	function isWordUsed(word: string): boolean {
+		return words.findIndex(w => w === word) < currentGameNo;
+	}
 
-  function showMe(): void{
-  	if (confirmedClicks !== 10) {
-  		++confirmedClicks;
-  		return;
-  	}
+	function showMe(): void{
+		if (confirmedClicks !== 10) {
+			++confirmedClicks;
+			return;
+		}
 
-  	if (confirm('Are you sure?')) {
-      confirmedClicks = 0;
-      alert(words[currentGameNo]);
-    }
-  }
+		if (confirm('Are you sure?')) {
+			confirmedClicks = 0;
+			alert(words[currentGameNo]);
+		}
+	}
 </script>
 
 <main>
-	<h1>
-    Word Finder
-    <span  title="sort {alphabetic?'alphabetically':'ranked'}" on:click="{() => alphabetic = !alphabetic}">
-    <svg xmlns="http://www.w3.org/2000/svg" class="sort-button {alphabetic ? 'ranked' :''}" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12" />
-    </svg>
-    </span>
-  </h1>
+	<h1>Word Finder</h1>
+	<p class="subtitle">
+		Game <span class="highlight" on:click={showMe}>{currentGameNo}</span> of {words.length}
+	</p>
 
-	<div style="margin-bottom: 1em;">
-		{#if hasSearched && !couldBe.length}
-			Could not find a match in word list.
-		{:else if couldBe.length}
-			{couldBe.length} options of {words.length} remain.
-		{:else}
-			There are {words.length} words in the list.<br>Best guess based on the scoring system is <span class="highlight">{bestWord}</span>
-		{/if}
+	<div>
+		<label>Correctly Placed Letters</label>
+		<input maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_0} on:keyup={lookFor}/>
+		<input maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_1} on:keyup={lookFor}/>
+		<input maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_2} on:keyup={lookFor}/>
+		<input maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_3} on:keyup={lookFor}/>
+		<input maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_4} on:keyup={lookFor}/>
 	</div>
 
 	<div>
-		<label for="correct">Correctly Placed Letters</label>
-		<input id="correct" type='text' maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_0} on:keyup={lookFor}/>
-		<input id="correct" type='text' maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_1} on:keyup={lookFor}/>
-		<input id="correct" type='text' maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_2} on:keyup={lookFor}/>
-		<input id="correct" type='text' maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_3} on:keyup={lookFor}/>
-		<input id="correct" type='text' maxlength="1" class="is" autocomplete="off" autocapitalize="off" spellcheck="false" use:lowercase bind:value={is_4} on:keyup={lookFor}/>
+		<label>Incorrectly Placed Letters</label>
+		<input maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_1} on:keyup={lookFor}/>
+		<input maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_0} on:keyup={lookFor}/>
+		<input maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_2} on:keyup={lookFor}/>
+		<input maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_3} on:keyup={lookFor}/>
+		<input maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_4} on:keyup={lookFor}/>
 	</div>
 
 	<div>
-		<label for="incorrect">Correct Letters not In Position</label>
-		<input id="incorrect" type='text' maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_1} on:keyup={lookFor}/>
-		<input id="incorrect" type='text' maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_0} on:keyup={lookFor}/>
-		<input id="incorrect" type='text' maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_2} on:keyup={lookFor}/>
-		<input id="incorrect" type='text' maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_3} on:keyup={lookFor}/>
-		<input id="incorrect" type='text' maxlength="4" class="almost" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={not_4} on:keyup={lookFor}/>
-	</div>
-
-	<div>
-		<label for="inCorrectLetters">Incorrect letters</label>
+		<label>Incorrect letters</label>
 		<input class="not" autocomplete="off" autocapitalize="off" autocorrect="off" spellcheck="false" use:lowercase bind:value={absent} on:keyup={lookFor}/>
 	</div>
 
-	<table>
-		{#if alphabetic}
-			{#each couldBe.sort() as word}
-				<tr>
-					<td class="{isWordUsed(word) ? 'used': ''}">{word}</td>
-				</tr>
-			{/each}
-		{:else}
-			{#each wordScores as word}
-				<tr>
-					<td class="{isWordUsed(word.word) ? 'used': ''}">{word.word}</td>
-				</tr>
-			{/each}
+	<div class="results">
+		<span>
+			{#if hasSearched && !couldBe.length}
+				Could not find a match in word list.
+			{/if}
+		</span>
+
+		{#each wordScores as word, i}
+			{#if showAll || i < 10}
+			<div>
+				<div>
+					<input readonly class="{word.word[0] === is_0 ? 'is' : (nots.includes(word.word[0]) && word.word[0] !== not_0 ? 'almost' : '')}" value={word.word[0]} />
+					<input readonly class="{word.word[1] === is_1 ? 'is' : (nots.includes(word.word[1]) && word.word[1] !== not_1 ? 'almost' : '')}" value={word.word[1]} />
+					<input readonly class="{word.word[2] === is_2 ? 'is' : (nots.includes(word.word[2]) && word.word[2] !== not_2 ? 'almost' : '')}" value={word.word[2]} />
+					<input readonly class="{word.word[3] === is_3 ? 'is' : (nots.includes(word.word[3]) && word.word[3] !== not_3 ? 'almost' : '')}" value={word.word[3]} />
+					<input readonly class="{word.word[4] === is_4 ? 'is' : (nots.includes(word.word[4]) && word.word[4] !== not_4 ? 'almost' : '')}" value={word.word[4]} />
+				</div>
+				{#if isWordUsed(word.word) }
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+					</svg>
+				{/if}
+			</div>
+			{:else if showAll || i === 10}
+				<label on:click={() => showAll = !showAll}>Click to show remaining {couldBe.length} options</label>
+			{/if}
+		{/each}
+		{#if showAll}
+			<label on:click={() => showAll = !showAll}>Click to only show top 10</label>
 		{/if}
-	</table>
-  <span class="bottom-right" style="opacity: 0.{confirmedClicks};" on:click={showMe}>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-    </svg>
-  </span>
+	</div>
 </main>
 
 <style>
-	label {
-		padding: 0.5em;
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
 	}
+
 	main {
 		text-align: center;
 		padding: 0 1em;
@@ -190,16 +179,47 @@
 		font-weight: 100;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	label {
+		padding: 0.5em 0 0.125em 0;
+		font-size: small;
+	}
+
+	input {
+		border: none;
+		text-align: center;
+		padding: 0.5em;
+	}
+
+	.subtitle {
+		font-size: smaller;
+		margin-top: -2em;
 	}
 
 	.highlight {
 		border-bottom: 1px solid #96CEB4;
 	}
-	
+
+	.results {
+		margin-top: 1em;
+		display: flex;
+		justify-content: space-around;
+		flex-direction: column;
+	}
+
+	.results>div {
+		display: flex;
+		justify-content: center;
+	}
+
+	.results input {
+		width: 2em;
+	}
+
+	.results svg {
+		width: 1.5em;
+		margin: -.5em -2em 0 .5em;
+	}
+
 	.is {
 		width: 2em;
 		background-color: #96CEB4;
@@ -211,30 +231,4 @@
 	.not {
 		background-color: #9D9D9D;
 	}
-	
-  .used {
-    opacity: 0.5;
-  }
-
-  .sort-button {
-    width: 2rem;
-    height: 2rem;
-    color: black;
-    transition-duration: 0.4s;
-    transition-property: transform;
-  }
-  .sort-button:hover {
-    opacity: 0.4;
-  }
-  .sort-button.ranked {
-    transform: rotate(180deg);
-  }
-
-  .bottom-right {
-    position: absolute;
-    bottom: 8px;
-    right: 8px;
-    height: 16px;
-    width: 16px;
-  }
 </style>
